94ef22d116d59f93cebdf9e01485f330db442c4f
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-29 21:51:56 +0300

    Installed redux and react-redux.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 import App from './App';
 
 import 'modern-normalize/modern-normalize.css';
@@ -7,7 +11,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
