b89d4b32f9af298c88fbd8fa07a0ac8c011e9f19
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-29 23:14:42 +0300

    Refactored App component.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,39 @@
-import React, { Component } from 'react';
+// Imports from React
+import React from 'react';
+// Imports of components
 import Section from './components/Section';
 import PageTitle from './components/PageTitle';
 import Title from './components/Title';
 import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList';
-class App extends Component {
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-  //   parsedContacts && this.setState({ contacts: [...parsedContacts] });
-  // }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.contacts !== this.state.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-
-  submitHandler = newContact => {
-    const { contacts } = this.state;
-    const existingContact = contacts.find(
-      contact => contact.name === newContact.name,
-    );
-    if (existingContact) {
-      alert(`${existingContact.name} is already in contacts.`);
-      return;
-    }
-    this.setState({ contacts: [newContact, ...contacts] });
-  };
-
-  filterUpdate = event => {
-    const { value } = event.currentTarget;
-
-    this.setState({
-      filter: value,
-    });
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId,
-        ),
-      };
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <PageTitle title="Phone Book" />
-        <Section>
-          <Title title="Add contacts" />
-          <ContactForm />
-          <Title title="Сontacts" />
-          <Filter />
-          <ContactList />
-        </Section>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <PageTitle title="Phone Book" />
+      <Section>
+        <Title title="Add contacts" />
+        <ContactForm />
+        <Title title="Сontacts" />
+        <Filter />
+        <ContactList />
+      </Section>
+    </>
+  );
+};
 
 export default App;
+
+// Left from class component till Local Storage refactoring
+// componentDidMount() {
+//   const contacts = localStorage.getItem('contacts');
+//   const parsedContacts = JSON.parse(contacts);
+//   parsedContacts && this.setState({ contacts: [...parsedContacts] });
+// }
+
+// componentDidUpdate(prevProps, prevState) {
+//   if (prevState.contacts !== this.state.contacts) {
+//     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//   }
+// }
