d5e39008eb5016805e8ec539d7d0f0e036128c20
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-30 00:45:30 +0300

    Added logger middleware to store config.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { phonebookReducer } from './phonebook-reducers';
+
+const middleware = [...getDefaultMiddleware(), logger];
 
 const store = configureStore({
   reducer: { contacts: phonebookReducer },
-  // middleware: '',
+  middleware,
   devTools: process.env.NODE_ENV !== 'production',
 });
 
