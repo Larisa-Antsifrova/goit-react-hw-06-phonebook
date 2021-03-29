d372d37c76d1f87c6929a16cc3f75a04f1113b94
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-29 21:53:15 +0300

    Added Provider component to the App.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+// Imports from React
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// Imports from Redux
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-
+// Imports of components
 import App from './App';
-
+// Styles imports
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 
