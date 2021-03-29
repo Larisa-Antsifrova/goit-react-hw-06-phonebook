a78ad69fdaf79fddfc22de2fda5c4b63f4b863ee
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-29 23:22:27 +0300

    Added conditional render.

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import styles from './PageTitle.module.css';
 
 const PageTitle = ({ title }) => {
-  return <h1 className={styles.title}>{title}</h1>;
+  return title && <h1 className={styles.title}>{title}</h1>;
 };
 
 PageTitle.defaultProps = {
